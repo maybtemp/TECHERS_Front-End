3690a8eabe29b132aa344547d1bd6da084daf660
MaSaOl1632 <mateusoliveira262010@gmail.com>
2026-05-21 15:56:11 -0300

    aaoulah

--- Aula 12/Aula 12.1/Popupinfo.js
+++ Aula 12/Aula 12.1/Popupinfo.js
@@ -23,55 +23,19 @@ class PopupInfo extends HTMLElement{
     constructor(){
         super()
         this.shadow = this.attachShadow({mode: 'open'})
+
+        const template = document.getElementById('popup-template')
+
+        this.shadow.appendChild(template.contentEditable.cloneNode(true))
+
     }
 
-    connectedCallback(){
-        const wrapper = document.createElement('span');
-        wrapper.setAttribute("class","wrapper");
-
-        const icon = document.createElement("span");
-        icon.setAttribute("class", "icon")
-        icon.setAttribute("tabindex", 0)
-
-        const info = document.createElement("span")
-        info.setAttribute("class", "info")
-
-    
-    const text = this.getAttribute("data-text");
-    info.textContent = text;
-    
-    
-    let imgUrl;
-    if(this.hasAttribute("img")){
-        imgUrl = this.getAttribute("img")
+    connectedCallback()
+    {
+        if(this.hasAttribute('img')){
+            const img = this.shadowRoot.querySelector('img')
+            img.src = this.getAttribute('img')
+        }
     }
-    else
-        {
-        imgUrl = "M/nao-grita-nao-grita.png" 
-    }
-
-    const img = document.createElement('img')
-    img.scr = img.Url;
-    img.setAttribute("alt","Icone de informacao")
-    img.setAttribute("width","100px")
-
-    const style = document.createElement("Style")
-    style.textContent = `
-    .wrapper{position: relative; display: inline-block;}
-    .info{
-    visibility: hidden; width; 200px; background-color: #232; color: #ffffff
-text-align: center; padding: 10px; border-radius: 6px; position: absolute; 
-z-index: 1; botton: 125%: left: 50%; margin-left: -100px; opacity: 100; transition:opacity 0.3s}
-    .icon hover + .info {visibility: visable; opacity: 1;}
-    `
-
-    icon.appendChild(img);
-    wrapper.appendChild(icon);
-    wrapper.appendChild(info);
-
-    this.shadow.appendChild(style);
-    this.shadow.appendChild(wrapper);
-    
-}
 }
 customElements.define("popup-info",PopupInfo )
---
+++ Aula 12/Aula 13/script.js
@@ -0,0 +1,7 @@
+const pg = document.getElementById('pagina')
+
+pg.addEventListener('submit', function(submit)
+{event.preventDefault()});
+  
+document.createElement(div)
+
