85549f637076c3a68f9a8c481315ac12f22030e2
MaSaOl1632 <mateusoliveira262010@gmail.com>
2026-05-28 15:55:56 -0300

    n sei

---
+++ Aula 12/Aula 13/Aula 13.1/script.js
@@ -0,0 +1,12 @@
+const txt = document.querySelector('.testo');
+const tema = document.querySelector('.texto')
+
+txt.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+});
+
+const principal = document.createElement(div)
+const nome = document.createElement(h3)
+const texto = document.createElement(p)
+const remover = document.createElement(button)
+
--- Aula 12/Aula 13/script.js
+++
@@ -1,7 +0,0 @@
-const pg = document.getElementById('pagina')
-
-pg.addEventListener('submit', function(submit)
-{event.preventDefault()});
-  
-document.createElement(div)
-
